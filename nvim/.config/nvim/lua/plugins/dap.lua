35782e5b4511b27c4de5520d16ed7b236115cd43
Snail Mail <>
2023-08-31 18:21:25 +0100

    NVIM: New DAP step over key binding

--- nvim/.config/nvim/lua/plugins/dap.lua
+++ nvim/.config/nvim/lua/plugins/dap.lua
@@ -12,6 +12,7 @@ return {
     { "<leader>dj", function() require("dap").down() end,                                                 desc = "Down" },
     { "<leader>dk", function() require("dap").up() end,                                                   desc = "Up" },
     { "<leader>dl", function() require("dap").run_last() end,                                             desc = "Run Last" },
+    { "<leader>dn", function() require("dap").step_over() end,                                            desc = "Step Over" },
     { "<leader>do", function() require("dap").step_out() end,                                             desc = "Step Out" },
     { "<leader>dO", function() require("dap").step_over() end,                                            desc = "Step Over" },
     { "<leader>dp", function() require("dap").pause() end,                                                desc = "Pause" },
