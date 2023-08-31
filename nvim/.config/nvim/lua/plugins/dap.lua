a8c16149d956da0783fcf09e4729d6cdb53b7272
Snail Mail <>
2023-08-31 18:09:01 +0100

    NVIM: DAP/RUST integration

--- nvim/.config/nvim/lua/plugins/dap.lua
+++ nvim/.config/nvim/lua/plugins/dap.lua
@@ -21,20 +21,6 @@ return {
     { "<leader>dw", function() require("dap.ui.widgets").hover() end,                                     desc = "Widgets" },
   },
 
-  config = function ()
-    local rt = require("rust-tools")
-    rt.setup({
-      server = {
-        on_attach = function(_, bufnr)
-          -- Hover actions
-          vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
-          -- Code action groups
-          vim.keymap.set("n", "<leader>ca", rt.code_action_group.code_action_group, { buffer = bufnr })
-        end,
-      },
-    })
-  end,
-
   dependencies = {
 
     -- fancy UI for the debugger
